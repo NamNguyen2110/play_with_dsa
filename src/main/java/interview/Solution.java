3ec3286899eaa3d0c5e599d4742c6e8d2af77cc1
namnguyen99 <nguyenvannamptit@gmail.com>
2021-07-15 23:18:45 +0700

    update

---
+++ src/main/java/interview/Solution.java
@@ -0,0 +1,10 @@
+package interview;
+
+import dsa.array.DynamicArray;
+
+public class Solution {
+    public static void main(String[] args) {
+        DynamicArray<String> dynamicArray = new DynamicArray<>();
+    }
+}
+
